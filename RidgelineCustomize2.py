bd5e6b565f0f2d3437811319c69e78bf920b069a
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-26 21:46:31 +0530

    Create RidgelineCustomize2.py

---
+++ RidgelineCustomize2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from joypy import joyplot
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Admission_Predict.csv")
+
+
+df['University Rating'] = df['University Rating'].astype(str)
+
+joyplot(df, by = 'University Rating', column = 'CGPA', colormap = cm.autumn, fade = True, range_style='own', figsize = (10,6), 
+        title = 'Distribution of Student CGPA based on University Rating')
+plt.xlabel("CGPA")
+
+
+plt.show()
