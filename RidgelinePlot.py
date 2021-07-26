97312d884cabe9e6b501fe1cc41ec2a9e3399bae
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-26 22:19:07 +0530

    Update RidgelinePlot.py

--- RidgelinePlot.py
+++ RidgelinePlot.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("Admission_Predict.csv")
 # print(df.info())
 
-df_new['University Rating'] = df_new['University Rating'].astype(str)
+df['University Rating'] = df['University Rating'].astype(str)
 
 joyplot(df, by = 'University Rating', column = 'CGPA')
 plt.xlabel("CGPA")
