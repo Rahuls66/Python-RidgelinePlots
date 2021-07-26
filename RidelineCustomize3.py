2ec431b38e5ea9e2c452addc450d5130fa1b32d9
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-26 22:24:54 +0530

    Create RidelineCustomize3.py

---
+++ RidelineCustomize3.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+from joypy import joyplot
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+df = pd.read_csv("Admission_Predict.csv")
+
+
+df['University Rating'] = df['University Rating'].astype(str)
+
+joyplot(df, by = 'University Rating', column = 'CGPA', color = 'Orange', fade = True, range_style='own', figsize = (10,6), hist = True, overlap = 0,
+                   title = 'Distribution of Student CGPA based on University Rating')
+plt.xlabel("CGPA")
+
+
+plt.show()
